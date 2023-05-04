b78e9b4a53f0b57cb6c46320a93ecd44d57ad3c5
yuyinws <lyc657508366@gmail.com>
2023-05-04 11:50:48 +0800

    chore: release

--- go.mod
+++ go.mod
@@ -5,6 +5,15 @@ go 1.20
 require github.com/urfave/cli/v2 v2.25.2
 
 require (
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/term v0.1.0 // indirect
+)
+
+require (
+	github.com/briandowns/spinner v1.23.0
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli v1.22.13
--- main.go
+++ main.go
@@ -5,27 +5,63 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
 
+	"github.com/briandowns/spinner"
 	"github.com/urfave/cli"
 )
 
 func main() {
 	app := &cli.App{
 		Name:  "got",
-		Usage: "fight the loneliness!",
-		Action: func(cCtx *cli.Context) error {
-			cmd := exec.Command("git", "clone", "https://github.com/yuyinws/yuyinws")
-			_, err := cmd.Output()
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println("clone end")
+		Usage: "Fast project scaffolding",
+		Action: func(ctx *cli.Context) error {
+			argUrl := ctx.Args().Get(0)
+			path := ctx.Args().Get(1)
+			repoName := parseUrl(argUrl)
 
+			if path == "" {
+				path = repoName
+			}
+
+			dir, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+			fullPath, err := filepath.Abs(filepath.Join(dir, path))
+			if err != nil {
+				return err
+			}
+
+			s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+			s.Start()
+
+			output, err := exec.Command("git", "clone", argUrl, path).CombinedOutput()
+			if err != nil {
+				return fmt.Errorf("%v: %s", err, output)
+			}
+
+			defer func() {
+				if err := os.RemoveAll(filepath.Join(fullPath, ".git")); err != nil {
+					log.Println(err)
+				}
+			}()
+			s.Stop()
+			fmt.Printf("Project success saved on \033[32m%s\033[0m", fullPath)
 			return nil
 		},
 	}
 
-	if err := app.Run(os.Args); err != nil {
+	err := app.Run(os.Args)
+	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func parseUrl(url string) string {
+	str := strings.Replace(url, ".git", "", -1)
+	arr := strings.Split(str, "/")
+	return arr[len(arr)-1]
 }
